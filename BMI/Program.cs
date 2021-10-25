cd5cfde8ba7a959df7000b278d30afa43178475d
Roman <Roman@DESKTOP-LKH8D4L>
2021-10-25 18:52:49 +0200

    Přidejte soubory projektu.

---
+++ BMI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulačka na BMI");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("napiš tvoji váhu kg:");
+            float váha = float.Parse(Console.ReadLine());
+            Console.WriteLine("Napiš svoji výšku m:");
+            float výška = float.Parse(Console.ReadLine());
+            float BMI = váha / (výška * výška);
+            Console.WriteLine("výpočet BMI: " + BMI);
+            Console.ReadKey();
+        }
+    }
+}
